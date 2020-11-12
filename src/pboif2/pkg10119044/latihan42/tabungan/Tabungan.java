4b8a344f5fdabe7a95d5eda33cc3bdb5fc54d874
ASUS <ASUS@ASUS-X541U>
2020-11-12 09:14:37 +0700

    Program ini bertujuan menampilkan hasil saldo tabungan

---
+++ src/pboif2/pkg10119044/latihan42/tabungan/Tabungan.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119044.latihan42.tabungan;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 
+ * NAMA     : Yohana Sri Rejeki
+ * KELAS    : IF2
+ * NIM      : 10119044
+ * Deskripsi Program : Program ini bertujuan menampilkan hasil saldo tabungan
+ */
+
+class Tabungan {
+    private int saldo;
+    Scanner input = new Scanner(System.in);
+    
+    public Tabungan(int saldo){
+        this.saldo = saldo;
+    }
+    
+    public int ambilUang(int jumlah){
+        System.out.print("Masukkan Saldo Awal : ");
+        saldo = input.nextInt();
+        System.out.print("Jumlah uang yang diambil : ");
+        jumlah = input.nextInt();
+        
+        if((saldo-jumlah)<=0){
+            System.out.println("Jumlah uang yang diambil harus lebih sedikit dari saldo Anda");
+        }else{
+            int saldoAkhir = saldo-jumlah;
+            System.out.println("Saldo Anda Sekarang : "+saldoAkhir);
+        }
+        
+        return jumlah;
+    }
+}
