749abb59aac32af7d5aa1fca8bbef171773a4edf
ASUS <ASUS@ASUS-X541U>
2020-11-12 09:14:29 +0700

    Program ini bertujuan menampilkan hasil saldo tabungan

---
+++ src/pboif2/pkg10119044/latihan42/tabungan/PBOIF210119044Latihan42Tabungan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119044.latihan42.tabungan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Yohana Sri Rejeki
+ * KELAS    : IF2
+ * NIM      : 10119044
+ * Deskripsi Program : Program ini bertujuan menampilkan hasil saldo tabungan
+ */
+
+public class PBOIF210119044Latihan42Tabungan {
+    public static void main(String[] args) {
+        Tabungan objTabungan = new Tabungan(0);
+        System.out.println("====Program Penarikan Uang====");
+        objTabungan.ambilUang(0);
+    }    
+}
